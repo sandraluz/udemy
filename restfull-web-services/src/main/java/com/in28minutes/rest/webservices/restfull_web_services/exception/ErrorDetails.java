556e181b33e9632462bc5399ff6ad147decbe2f2
DESKTOP-33H5VVH\matis <matis.luz@gmail.com>
2025-09-08 20:30:55 +0200

    Handle exceptions

---
+++ restfull-web-services/src/main/java/com/in28minutes/rest/webservices/restfull_web_services/exception/ErrorDetails.java
@@ -0,0 +1,42 @@
+package com.in28minutes.rest.webservices.restfull_web_services.exception;
+
+import java.time.LocalDateTime;
+
+public class ErrorDetails {
+	private LocalDateTime timespamp;
+	private String message;
+	private String details;
+
+	public ErrorDetails(LocalDateTime timespamp, String message, String details) {
+		super();
+		this.timespamp = timespamp;
+		this.message = message;
+		this.details = details;
+	}
+
+	public LocalDateTime getTimespamp() {
+		return timespamp;
+	}
+
+	public void setTimespamp(LocalDateTime timespamp) {
+		this.timespamp = timespamp;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+
+}
