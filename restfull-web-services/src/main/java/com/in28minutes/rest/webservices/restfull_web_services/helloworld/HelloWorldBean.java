6201ae37586ea85e6a6ef0adeaa3afde2cb8c202
DESKTOP-33H5VVH\matis <matis.luz@gmail.com>
2025-09-08 20:04:34 +0200

    first commit

---
+++ restfull-web-services/src/main/java/com/in28minutes/rest/webservices/restfull_web_services/helloworld/HelloWorldBean.java
@@ -0,0 +1,19 @@
+package com.in28minutes.rest.webservices.restfull_web_services.helloworld;
+
+public class HelloWorldBean {
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
---
+++ restfull-web-services/src/main/java/com/in28minutes/rest/webservices/restfull_web_services/user/UserDaoService.java
@@ -0,0 +1,36 @@
+package com.in28minutes.rest.webservices.restfull_web_services.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class UserDaoService {
+	private static List<User> users = new ArrayList<User>();
+	private static int usersCount = 0;
+
+	static {
+		users.add(new User(++usersCount, "Adm", LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount, "Eva", LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount, "Jim", LocalDate.now().minusYears(30)));
+	}
+
+	public List<User> findAll() {
+		return users;
+	}
+
+	public User save(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
+	}
+
+	public User findOne(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+		return users.stream().filter(predicate).findFirst().orElse(null);
+	}
+}
