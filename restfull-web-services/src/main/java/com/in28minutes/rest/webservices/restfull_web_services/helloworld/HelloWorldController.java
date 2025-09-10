4714f7932d591e68f2f7ea4bbb412b2a25a5fd57
DESKTOP-33H5VVH\matis <matis.luz@gmail.com>
2025-09-10 21:54:50 +0200

    Added i18n

--- restfull-web-services/src/main/java/com/in28minutes/rest/webservices/restfull_web_services/helloworld/HelloWorldController.java
+++ restfull-web-services/src/main/java/com/in28minutes/rest/webservices/restfull_web_services/helloworld/HelloWorldController.java
@@ -1,15 +1,29 @@
 package com.in28minutes.rest.webservices.restfull_web_services.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	private MessageSource messageSource;
 
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	@GetMapping(path = "hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+
+	@GetMapping(path = "hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
 	}
 
 	@GetMapping(path = "hello-world-bean")
